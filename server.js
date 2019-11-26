35a2815c06ec9319a3942fdf8533b9aa83dfa545
Szilard Udroiu <uszilard@gmail.com>
2019-11-26 19:57:52 +0100

    smallchanges

--- server.js
+++ server.js
@@ -7,7 +7,36 @@ var PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var tables = [
+    {
+        customerName: "Pablo",
+        phoneNumber: "0123456789",
+        customerEmail: "pablo@email.com",
+        customerID: "1234"
+    }
+];
 
+var waitlist = [
+    {
+        customerName: "Pablo",
+        phoneNumber: "0123456789",
+        customerEmail: "pablo@email.com",
+        customerID: "1234"
+    }
+];
+
+app.get("/", function (req, res) {
+    console.log("hey i suck")
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
 app.listen(PORT, function () {
 
