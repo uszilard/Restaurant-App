3dca8016d4d1e4c7ef18932511c7fb1d6a6c1ff5
Szilard Udroiu <uszilard@gmail.com>
2019-11-26 21:06:29 +0100

    smallchanges

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
 
+////////////////////////////////////
 var app = express();
 var PORT = 8080;
 
+////////////////////////////////////
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+////////////////////////////////////
 var tables = [
     {
         customerName: "Pablo",
@@ -25,6 +28,7 @@ var waitlist = [
     }
 ];
 
+////////////////////////////////////
 app.get("/", function (req, res) {
     console.log("hey i work")
     res.sendFile(path.join(__dirname, "home.html"));
@@ -38,9 +42,7 @@ app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-
-
-//
+////////////////////////////////////
 app.get("/api/tables", function (req, res) {
     res.json(tables);
 });
@@ -49,29 +51,33 @@ app.get("/api/waitlist", function (req, res) {
     res.json(waitlist);
 });
 
+////////////////////////////////////
 app.post("/api/tables", function (req, res) {
     var newReservation = req.body
 
 
+    if (tables.length > 5) {
+        waitlist.push(newReservation);
 
-    tables.push(newReservation);
+    } else {
+        tables.push(newReservation)
+    }
 
     res.json(newReservation);
 
 });
 
 app.post("/api/waitlist", function (req, res) {
-    console.log("request", req)
+
     var newWaitList = req.body
 
-    tables.push(newWaitList);
+    waitlist.push(newWaitList);
 
     res.json(newWaitList);
 
 });
 
-
-//
+////////////////////////////////////
 app.listen(PORT, function () {
 
     console.log("Server listening on: http://localhost:" + PORT);
